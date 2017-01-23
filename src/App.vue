e2a18ed555879da2f90025725a0ad0521ff68547
Azlan Cuttilan <azlan@bynd.com>
2017-01-23 11:09:55 +0000

    Exercise 2 global filter
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,14 @@
                 <h1>Filters & Mixins</h1>
                 <!-- Exercise 1) -->
                 <!-- Build a local Filter which reverses the Text it is applied on -->
+                <h2>Exercise 1</h2>
                 <p>{{ text | reverse }}</p>
 
                 <!-- Exercise 2 -->
                 <!-- Build a global Filter which counts the length of a word and it appends it -->
                 <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+               <h2>Exercise 2</h2>
+                <p>{{ text | str-length }}</p>
 
                 <!-- Exercise 3 -->
                 <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
